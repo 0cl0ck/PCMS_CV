72935651e988ab3866a33c59a9fa9d81679af029
0cl0ck <hugodewas@gmail.com>
2025-01-15 18:44:08 +0100

    prettier formating

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -15,14 +15,18 @@ export const Logo = (props: Props) => {
   return (
     /* eslint-disable @next/next/no-img-element */
     <img
-      alt="Payload Logo"
+      alt="Chanvre Vert Logo"
       width={193}
       height={34}
       loading={loading}
       fetchPriority={priority}
       decoding="async"
-      className={clsx('max-w-[9.375rem] w-full h-[34px]', className)}
-      src="/media/logo.png"
+      className={clsx(
+        'max-w-[12rem] w-full h-[34px]',
+        'transition-transform duration-300 hover:scale-105', // Ajout de l'animation
+        className,
+      )}
+      src="/media/logo_03.png"
     />
   );
 };
