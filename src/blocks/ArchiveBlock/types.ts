cc721a4521bcb51bdc4a00f3ce6c4abcc48bc270
0cl0ck <hugodewas@gmail.com>
2025-02-26 16:53:18 +0100

    Fix: Remove any from ArchiveBlock types

--- src/blocks/ArchiveBlock/types.ts
+++ src/blocks/ArchiveBlock/types.ts
@@ -1,24 +1,15 @@
-import type { Post, Product, ProductCategory } from '@/payload-types';
+import type {
+  ArchiveBlock as ArchiveBlockType,
+  Post,
+  Product,
+  ProductCategory,
+} from '@/payload-types';
 
 export interface ArchiveBlockSelectedDoc {
   relationTo: 'posts' | 'products' | 'product-categories';
   value: Post | Product | ProductCategory;
 }
 
-export interface ArchiveBlock {
-  id?: string;
-  blockType: 'archive';
-  introContent?: any;
-  populateBy: 'collection' | 'selection';
-  relationTo?: 'posts' | 'products' | 'product-categories';
-  categories?: Array<{
-    id: string;
-    [key: string]: any;
-  }>;
-  productCategories?: Array<{
-    id: string;
-    [key: string]: any;
-  }>;
-  limit?: number;
+export interface ArchiveBlock extends Omit<ArchiveBlockType, 'selectedDocs'> {
   selectedDocs?: ArchiveBlockSelectedDoc[];
 }
