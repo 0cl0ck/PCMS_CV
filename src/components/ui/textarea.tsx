2578334b5b74ab39464f68bf9df952b29551ed31
0cl0ck <hugodewas@gmail.com>
2025-02-26 23:15:49 +0100

    Fix: Deleted interface TextAreaProps in ui/textarea.tsx

--- src/components/ui/textarea.tsx
+++ src/components/ui/textarea.tsx
@@ -1,13 +1,12 @@
 import * as React from 'react';
 import { cn } from 'src/utilities/cn';
 
-export interface TextareaProps extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {}
-
-const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
-  ({ className, ...props }, ref) => {
-    return <textarea className={cn(className)} ref={ref} {...props} />;
-  },
-);
+const Textarea = React.forwardRef<
+  HTMLTextAreaElement,
+  React.TextareaHTMLAttributes<HTMLTextAreaElement>
+>(({ className, ...props }, ref) => {
+  return <textarea className={cn(className)} ref={ref} {...props} />;
+});
 Textarea.displayName = 'Textarea';
 
 export { Textarea };
