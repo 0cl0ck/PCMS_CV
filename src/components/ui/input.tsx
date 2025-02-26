9ec272932986f51d99c5bf3917b9ff35d7f436bd
0cl0ck <hugodewas@gmail.com>
2025-02-26 23:14:41 +0100

    Fix: Deleted interface InputProps in /ui/input.tsx

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -1,9 +1,7 @@
 import * as React from 'react';
 import { cn } from 'src/utilities/cn';
 
-export interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {}
-
-const Input = React.forwardRef<HTMLInputElement, InputProps>(
+const Input = React.forwardRef<HTMLInputElement, React.InputHTMLAttributes<HTMLInputElement>>(
   ({ type, className, ...props }, ref) => {
     return (
       <input
